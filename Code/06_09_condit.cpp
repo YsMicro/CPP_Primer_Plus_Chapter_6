86232ebe93157e4ebe4b72a5a0a4677e7bec3020
Vojago <ywh2742450799@gmail.com>
2024-04-01 20:38:07 +0800

    完成06.09

--- Code/06_09_condit.cpp
+++ Code/06_09_condit.cpp
@@ -6,11 +6,11 @@
 [[maybe_unused]] int code_09() {
     using namespace std;
     int a,b;
-    int c = 0;
+
     cout << "输入两个整数：" << endl;
     cin >> a >> b;
     cout << a << " 和 " << b << " 中的较大者";
-    c = a > b ? a : b;
+    int c = a > b ? a : b;
     cout << "为 " << c <<endl;
     return 0;
 }
